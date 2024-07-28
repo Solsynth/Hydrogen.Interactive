7ae1f8021baaa3e298ee834a28187c86d6c50a1c
LittleSheep <littlesheep.code@hotmail.com>
2024-07-28 22:01:24 +0800

    :bug: Try to fix the language detect issue

--- pkg/internal/services/languages.go
+++ pkg/internal/services/languages.go
@@ -6,12 +6,20 @@ import (
 	"github.com/pemistahl/lingua-go"
 )
 
-func DetectLanguage(content string) string {
-	return "unknown"
+var detector lingua.LanguageDetector
 
-	detector := lingua.NewLanguageDetectorBuilder().
-		FromLanguages(lingua.AllLanguages()...).
+func CreateLanguageDetector() lingua.LanguageDetector {
+	return lingua.NewLanguageDetectorBuilder().
+		FromAllLanguages().
+		WithLowAccuracyMode().
 		Build()
+}
+
+func DetectLanguage(content string) string {
+	if detector == nil {
+		detector = CreateLanguageDetector()
+	}
+
 	if lang, ok := detector.DetectLanguageOf(content); ok {
 		return strings.ToLower(lang.String())
 	}
